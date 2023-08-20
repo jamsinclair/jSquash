19f17ae1c0ccff30083f385aef0131fba75f051a
Jamie <5964236+jamsinclair@users.noreply.github.com>
2023-08-20 17:20:26 +0000

    wip 2

--- packages/oxipng/codec/pkg-parallel/squoosh_oxipng.d.ts
+++ packages/oxipng/codec/pkg-parallel/squoosh_oxipng.d.ts
@@ -4,9 +4,10 @@
 * @param {Uint8Array} data
 * @param {number} level
 * @param {boolean} interlace
+* @param {boolean} optimize_alpha
 * @returns {Uint8Array}
 */
-export function optimise(data: Uint8Array, level: number, interlace: boolean): Uint8Array;
+export function optimise(data: Uint8Array, level: number, interlace: boolean, optimize_alpha: boolean): Uint8Array;
 /**
 * @param {number} num_threads
 * @returns {Promise<any>}
@@ -36,20 +37,32 @@ export class wbg_rayon_PoolBuilder {
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
-  readonly __wasm_init_memory: () => void;
-  readonly optimise: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly optimise: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
   readonly __wbg_wbg_rayon_poolbuilder_free: (a: number) => void;
   readonly wbg_rayon_poolbuilder_numThreads: (a: number) => number;
   readonly wbg_rayon_poolbuilder_receiver: (a: number) => number;
   readonly wbg_rayon_poolbuilder_build: (a: number) => void;
   readonly initThreadPool: (a: number) => number;
   readonly wbg_rayon_start_worker: (a: number) => void;
-  readonly __wbindgen_export_0: WebAssembly.Memory;
+  readonly memory: WebAssembly.Memory;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
-  readonly __wbindgen_malloc: (a: number) => number;
-  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_thread_destroy: (a: number, b: number) => void;
   readonly __wbindgen_start: () => void;
 }
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput, maybe_memory?: WebAssembly.Memory): InitOutput;
 
 /**
 * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
@@ -60,4 +73,4 @@ export interface InitOutput {
 *
 * @returns {Promise<InitOutput>}
 */
-export default function init (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput>;
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput>;
