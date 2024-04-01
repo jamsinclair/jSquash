2183d508b4aed9c836250451a8df3eda63156494
Jamie Sinclair <jamsinclairnz@gmail.com>
2024-04-01 15:33:25 +0900

    test(png): add tests for the decoding of non-rgba png files

--- test/node/png.test.js
+++ test/node/png.test.js
@@ -28,6 +28,42 @@ test('can successfully decode png with invalid ICC profile checksum', async (t) 
   t.is(data.data.length, 4 * 16 * 16);
 });
 
+test('can successfully decode png with no alpha', async (t) => {
+  const [testImage, decodeWasmModule] = await Promise.all([
+    getFixturesImage('test-rgb.png'),
+    importWasmModule('node_modules/@jsquash/png/codec/pkg/squoosh_png_bg.wasm'),
+  ]);
+  initDecode(decodeWasmModule);
+  const data = await decode(testImage);
+  t.is(data.width, 50);
+  t.is(data.height, 50);
+  t.is(data.data.length, 4 * 50 * 50);
+});
+
+test('can successfully decode grayscale png with no alpha', async (t) => {
+  const [testImage, decodeWasmModule] = await Promise.all([
+    getFixturesImage('test-grayscale.png'),
+    importWasmModule('node_modules/@jsquash/png/codec/pkg/squoosh_png_bg.wasm'),
+  ]);
+  initDecode(decodeWasmModule);
+  const data = await decode(testImage);
+  t.is(data.width, 50);
+  t.is(data.height, 50);
+  t.is(data.data.length, 4 * 50 * 50);
+});
+
+test('can successfully decode grayscale png with alpha', async (t) => {
+  const [testImage, decodeWasmModule] = await Promise.all([
+    getFixturesImage('test-grayscale-alpha.png'),
+    importWasmModule('node_modules/@jsquash/png/codec/pkg/squoosh_png_bg.wasm'),
+  ]);
+  initDecode(decodeWasmModule);
+  const data = await decode(testImage);
+  t.is(data.width, 50);
+  t.is(data.height, 50);
+  t.is(data.data.length, 4 * 50 * 50);
+});
+
 test('can successfully encode image', async (t) => {
   const encodeWasmModule = await importWasmModule(
     'node_modules/@jsquash/png/codec/pkg/squoosh_png_bg.wasm',
