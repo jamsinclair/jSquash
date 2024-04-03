0a6d8a729c2dc0be5ea1189cd880bafc5fea0d12
Jamie Sinclair <jamsinclairnz@gmail.com>
2024-04-03 13:50:06 +0900

    feat(oxipng): upgrade to v9 and add optimise_raw method

--- packages/oxipng/codec/src/lib.rs
+++ packages/oxipng/codec/src/lib.rs
@@ -1,14 +1,41 @@
 #[cfg(feature = "parallel")]
 pub use wasm_bindgen_rayon::init_thread_pool;
 
-use oxipng::Interlacing;
+use oxipng::{BitDepth, ColorType, Interlacing};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::Clamped;
 
 #[wasm_bindgen]
 pub fn optimise(data: &[u8], level: u8, interlace: bool, optimize_alpha: bool) -> Vec<u8> {
     let mut options = oxipng::Options::from_preset(level);
-    options.interlace = Some(if interlace { Interlacing::Adam7 } else { Interlacing::None });
+    options.interlace = Some(if interlace {
+        Interlacing::Adam7
+    } else {
+        Interlacing::None
+    });
     options.optimize_alpha = optimize_alpha;
 
     oxipng::optimize_from_memory(data, &options).unwrap_throw()
+}
+
+#[wasm_bindgen]
+pub fn optimise_raw(
+    data: Clamped<Vec<u8>>,
+    width: u32,
+    height: u32,
+    level: u8,
+    interlace: bool,
+    optimize_alpha: bool,
+) -> Vec<u8> {
+    let mut options = oxipng::Options::from_preset(level);
+    options.interlace = Some(if interlace {
+        Interlacing::Adam7
+    } else {
+        Interlacing::None
+    });
+    options.optimize_alpha = optimize_alpha;
+
+    let raw = oxipng::RawImage::new(width, height, ColorType::RGBA, BitDepth::Eight, data.0)
+        .unwrap_throw();
+    raw.create_optimized_png(&options).unwrap_throw()
 }
