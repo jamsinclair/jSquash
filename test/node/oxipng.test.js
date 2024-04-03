80ec4f1b9a61dc7e8665ac2096d62c8585a87322
Jamie Sinclair <jamsinclairnz@gmail.com>
2024-04-03 15:38:22 +0900

    test(oxipng): add integration test for oxipng with raw image data

--- test/node/oxipng.test.js
+++ test/node/oxipng.test.js
@@ -15,3 +15,18 @@ test('can successfully optimise png image', async (t) => {
   t.assert(optimisedImage instanceof ArrayBuffer);
   t.assert(optimisedImage.byteLength < testImage.byteLength);
 });
+
+test('can successfully optimise from raw image data and output png image', async (t) => {
+  const optimiseWasmModule = await importWasmModule(
+    'node_modules/@jsquash/oxipng/codec/pkg/squoosh_oxipng_bg.wasm',
+  );
+  await init(optimiseWasmModule);
+
+  const testRawImageData = new ImageData(
+    new Uint8ClampedArray(4 * 50 * 50),
+    50,
+    50,
+  );
+  const optimisedImage = await optimise(testRawImageData);
+  t.assert(optimisedImage instanceof ArrayBuffer);
+});
