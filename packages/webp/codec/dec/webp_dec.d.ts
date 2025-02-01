185bd4c77b4a510636864cfd5739c06d8d3c61f1
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-02-01 17:45:10 +0900

    add compiled files

--- packages/webp/codec/dec/webp_dec.d.ts
+++ packages/webp/codec/dec/webp_dec.d.ts
@@ -1,6 +1,9 @@
 export interface WebPModule extends EmscriptenWasm.Module {
   decode(data: BufferSource): ImageData | null;
+  decodeAnimated(data: BufferSource): WebPFrame[] | null;
 }
+
+export type WebPFrame = { imageData: ImageData, duration: number };
 
 declare var moduleFactory: EmscriptenWasm.ModuleFactory<WebPModule>;
 
