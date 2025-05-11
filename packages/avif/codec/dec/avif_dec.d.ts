75f4b78c92cab682fa3198a59fba4378968700d7
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-05-11 17:15:49 +0900

    chore: update types

--- packages/avif/codec/dec/avif_dec.d.ts
+++ packages/avif/codec/dec/avif_dec.d.ts
@@ -1,7 +1,7 @@
 export interface AVIFModule extends EmscriptenWasm.Module {
-  decode(data: BufferSource, bitDepth: 10 | 12): { data: Uint16Array, height: number, width: number } | null;
+  decode(data: BufferSource, bitDepth: 10 | 12 | 16): { data: Uint16Array, height: number, width: number } | null;
   decode(data: BufferSource, bitDepth: 8): ImageData | null;
-  decode(data: BufferSource, bitDepth: 8 | 10 | 12): { data: Uint16Array, height: number, width: number } | ImageData | null;
+  decode(data: BufferSource, bitDepth: 8 | 10 | 12 | 16): { data: Uint16Array, height: number, width: number } | ImageData | null;
 }
 
 declare var moduleFactory: EmscriptenWasm.ModuleFactory<AVIFModule>;
