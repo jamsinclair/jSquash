4343734a8b742cb66b15e5c243f6d541e681987d
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-05-13 00:12:56 +0900

    chore: fix up netlify example app and bump to latest pkg versions

--- examples/web-workers/vite.config.js
+++ examples/web-workers/vite.config.js
@@ -4,6 +4,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["@jsquash/avif", "@jsquash/jpeg", "@jsquash/jxl", "@jsquash/png", "@jsquash/webp"]
   },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  },
   worker: {
     format: 'es'
   }
